4ef239149b8b45aa0677e0e8550fbc9988d6a236
cyrusrefahi@gmail.com <cyrusrefahi@gmail.com>
2023-10-06 19:00:11 -0700

    +Adds test target to project

---
+++ UnitTest/ConversationsTests.swift
@@ -0,0 +1,16 @@
+//
+//  ConversationsTests.swift
+//  AITherapistTests
+//
+//  Created by cyrus refahi on 10/6/23.
+//
+
+import XCTest
+
+final class ConversationsTests: XCTestCase {
+
+    func testConversation()  {
+        XCTAssertEqual(1, 1)
+    }
+
+}
