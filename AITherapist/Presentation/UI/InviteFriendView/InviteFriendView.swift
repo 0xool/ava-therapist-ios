f42e03146bcf1948cb33ac1a6ebb5a56ae114ea2
cyrusrefahi@gmail.com <cyrusrefahi@gmail.com>
2024-01-20 12:02:55 -0800

    Add new assets and update file names

---
+++ AITherapist/Presentation/UI/InviteFriendView/InviteFriendView.swift
@@ -0,0 +1,18 @@
+//
+//  InviteFriendView.swift
+//  AITherapist
+//
+//  Created by cyrus refahi on 1/20/24.
+//
+
+import SwiftUI
+
+struct InviteFriendView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InviteFriendView()
+}
